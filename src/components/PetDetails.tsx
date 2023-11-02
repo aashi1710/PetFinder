d8b77e73f936fa42f1c242d8953efd6c4d3d51cf
aashi1710 <98670263+aashi1710@users.noreply.github.com>
2023-11-02 16:28:36 +0530

    Add files via upload

---
+++ src/components/PetDetails.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Pet } from "../models/Pet";
+import { getPetById } from "../services/petService";
+import { Card, Col, Row } from "react-bootstrap";
+
+export function PetDetails() {
+  const { id } = useParams();
+  const [pet, setPet] = useState<Pet | null>(null);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      getPetById(Number(id)).then((pet) => {
+        setPet(pet);
+      });
+    }
+  }, [id]);
+
+  return (
+    <div className="PetDetails">
+      {pet && (
+        <Row>
+          <Col lg={2}></Col>
+          <Col lg={8}>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col lg={4}>
+                    <img src={`/img/${pet.image}`}></img>
+                  </Col>
+                  <Col lg={8}>
+                    <Card.Title>{"Name:" + pet.name}</Card.Title>
+                    <Card.Text>
+                      {"Breed: " + pet.breed} -{" "}
+                      {"Gender: " + pet.isBoy ? "Boy" : "Girl"}
+                    </Card.Text>
+                    <Card.Text>
+                      {"Vaccinated: " + pet.isVaccinated ? "Yes" : "False"}
+                    </Card.Text>
+                    <Card.Text>{pet.description}</Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={2}></Col>
+        </Row>
+      )}
+    </div>
+  );
+}
